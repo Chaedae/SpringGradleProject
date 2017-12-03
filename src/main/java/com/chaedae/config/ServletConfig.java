f1b6b1672eaca13ea74778a8689391bf92235b23
ChaeDae <chaedae0@gmail.com>
2017-12-03 17:24:23 +0900

    Add to Spring Configuration

---
+++ src/main/java/com/chaedae/config/ServletConfig.java
@@ -0,0 +1,50 @@
+package com.chaedae.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Spring WebMVC Configuration
+ * @author ChaeDae
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.chaedae"})
+public class ServletConfig implements WebMvcConfigurer {
+
+	/**
+	 * ViewResolver Configuration
+	 * @return viewResolver
+	 */
+	@Bean
+	public InternalResourceViewResolver resolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		
+		return resolver;
+	}
+	
+	/**
+	 * MessageSource Configuration
+	 * 다국어 처리
+	 * @return
+	 */
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("messages");
+		
+		return source;
+	}
+	
+	
+}
